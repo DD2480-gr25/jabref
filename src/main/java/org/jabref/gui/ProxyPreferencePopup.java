5300ada4cb9ce39fb908a4ff53bde21dc0f11449
Emilia <34159740+emiros98@users.noreply.github.com>
2022-03-09 18:06:57 +0100

    Feature #49 - Add class for passowrd prompt

--- src/main/java/org/jabref/gui/ProxyPreferencePopup.java
+++ src/main/java/org/jabref/gui/ProxyPreferencePopup.java
@@ -1,2 +1,20 @@
-package org.jabref.gui;public class ProxyPreferenceChecker {
+package org.jabref.gui;
+
+import org.jabref.logic.l10n.Localization;
+import org.jabref.preferences.PreferencesService;
+
+import java.util.Optional;
+
+public class ProxyPreferencePopup {
+
+    public static void askForPasswordAndWait(DialogService dialogService, PreferencesService preferences) {
+        Optional<String> password = dialogService.showInputDialogWithDisableAndWait(
+                Localization.lang("No proxy password found"),
+                Localization.lang("Please enter password"),
+                Localization.lang("Disable proxy"),
+                preferences.getProxyPreferences()::setUseProxy);
+        password.ifPresent(s -> preferences.getPasswordProvider().set(s));
+
+    }
+
 }
